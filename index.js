c3cd87263a780684e0f2bcca46cd47bae0d16ffb
Guilherme2660 <guilherme.l05@aluno.ifsc.edu.br>
2023-06-16 22:41:41 -0300

    mudança

---
+++ index.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var text = document.querySelector('.text');
+
+    // Adiciona a classe 'active' para animar o texto
+    text.classList.add('active');
+  });
